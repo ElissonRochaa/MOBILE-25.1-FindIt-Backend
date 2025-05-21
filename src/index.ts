19820d04c7db34bb73e45f5e3c2207f8c3408b75
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2025-05-21 16:01:05 -0300

    feat: new routes to aindex.ts

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,24 @@
-
 import express, { Request, Response } from "express";
-import { connectToDatabase } from "./config/database"; 
+import { connectToDatabase } from "./config/database";
+import authRouter from "./routes/authRoutes";
+import cors from 'cors';
 
 const app = express();
 const PORT = 8080;
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Olá, backend do FindIt rodando!");
 });
 
-const startServer = async () => {
-  await connectToDatabase();
+app.use('/api/v1/auth', authRouter); 
 
+const startServer = async () => {
+  await connectToDatabase(); 
   app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+    console.log(` Servidor rodando em http://localhost:${PORT}`);
   });
 };
 
