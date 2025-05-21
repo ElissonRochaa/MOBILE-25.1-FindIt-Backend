d366625465bfb13952bc2f2390d339fcfe4573e1
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2025-05-21 14:58:55 -0300

    feat:  added connectToDataBase fn

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+
 import express, { Request, Response } from "express";
+import { connectToDatabase } from "./config/database"; 
 
 const app = express();
 const PORT = 8080;
@@ -9,6 +11,12 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Olá, backend do FindIt rodando!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
+const startServer = async () => {
+  await connectToDatabase();
+
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+  });
+};
+
+startServer();
