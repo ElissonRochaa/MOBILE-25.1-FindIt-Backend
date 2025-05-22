37565bd7d4eb67d9416bd8e1cab0433c88512957
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2025-05-22 16:48:22 -0300

    feat: static archives

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,16 @@ import { connectToDatabase } from "./config/database";
 import authRouter from "./routes/authRoutes";
 import cors from 'cors';
 import postRouter from "./routes/postRoutes";
+import path from 'path';  // Importa o path aqui
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
+
+
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Olá, backend do FindIt rodando!");
@@ -20,7 +24,7 @@ app.use('/api/v1/posts', postRouter);
 const startServer = async () => {
   await connectToDatabase(); 
   app.listen(PORT, () => {
-    console.log(` Servidor rodando em http://localhost:${PORT}`);
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
   });
 };
 
