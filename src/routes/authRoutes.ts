55edf2e5a53c711e53fc99ad1ae82a921c8063e9
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2025-05-22 21:28:39 -0300

    feat: new route

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { loginUser, registerUser } from '../controllers/authController';
+import { loginUser, registerUser, getUserById } from '../controllers/authController';
 
 const authRouter = Router(); 
 
 authRouter.post('/register', registerUser);
 authRouter.post('/login', loginUser);
+authRouter.get('/user/:id', getUserById);
 
-export default authRouter; 
+export default authRouter;
