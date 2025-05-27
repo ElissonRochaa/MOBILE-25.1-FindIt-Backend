34551d88dd4885b0bb8a3b2a2f548b7613071e88
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2025-05-26 21:58:17 -0300

    refactor: Post model

--- src/models/Post.ts
+++ src/models/Post.ts
@@ -4,10 +4,10 @@ export interface IPost extends Document {
   _id: mongoose.Types.ObjectId;
   nomeItem: string;
   descricao: string;
-  data: Date; 
-  situacao: 'achado' | 'perdido'; 
-  fotoUrl?: string; // URL da foto (opcional, após upload)
-  usuario: mongoose.Types.ObjectId; // ID do usuário que criou o post
+  data: Date;
+  situacao: 'achado' | 'perdido';
+  fotoUrl?: string;
+  usuario: mongoose.Types.ObjectId;
   createdAt: Date;
   updatedAt: Date;
   __v: number;
@@ -30,23 +30,22 @@ const PostSchema: Schema = new Schema({
   },
   situacao: {
     type: String,
-    enum: ['achado', 'perdido'], 
+    enum: ['achado', 'perdido'],
     required: [true, 'A situação é obrigatória (achado/perdido).'],
   },
   fotoUrl: {
     type: String,
-    required: false, // Será preenchido após o upload da imagem
+    required: false,
   },
   usuario: {
     type: Schema.Types.ObjectId,
-    ref: 'User', // Faz referência ao modelo 'User'
+    ref: 'User',
     required: [true, 'O usuário criador do post é obrigatório.'],
   },
 }, {
-  timestamps: true, 
+  timestamps: true,
 });
 
-// 3. Criação e Exportação do Modelo
 const Post = mongoose.model<IPost>('Post', PostSchema);
 
 export default Post;
