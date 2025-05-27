96ea536b04f0f509a8a63535bfe877b343982e4e
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2025-05-26 22:16:27 -0300

    refactor: index.ts now handle with new routes

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,41 @@
 import express, { Request, Response } from "express";
-import { connectToDatabase } from "./config/database";
-import authRouter from "./routes/authRoutes";
 import cors from 'cors';
+import http from 'http';
+import path from 'path';
+
+import { connectToDatabase } from "./config/database";
+import { initSocket } from './socket';
+
+import authRouter from "./routes/authRoutes";
 import postRouter from "./routes/postRoutes";
-import path from 'path';  // Importa o path aqui
+import userRouter from './routes/userRoutes';
+import conversationRouter from './routes/conversationRoutes';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+const server = http.createServer(app);
+
+initSocket(server);
+
 app.use(cors());
 app.use(express.json());
-
 
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Olá, backend do FindIt rodando!");
+  res.send("Olá, backend do FindIt rodando com chat em tempo real!");
 });
 
 app.use('/api/v1/auth', authRouter); 
 app.use('/api/v1/posts', postRouter); 
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/conversations', conversationRouter);
 
 const startServer = async () => {
   await connectToDatabase(); 
-  app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
+  server.listen(PORT, () => {
+    console.log(`🚀 Servidor rodando em http://localhost:${PORT}`);
   });
 };
 
